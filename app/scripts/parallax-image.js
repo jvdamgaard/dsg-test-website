52b41dfaa3ae7d9bd6bc0c01d138d1d285c5207f
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2015-03-14 16:20:56 +0100

    Only change visible images

--- app/scripts/parallax-image.js
+++ app/scripts/parallax-image.js
@@ -4,17 +4,35 @@
 
     var setContainerHeight = function() {
         [].forEach.call($parallaxImages, function($parallaxImage) {
-            $parallaxImage.$container.height = Math.ceil(window.innerHeight * 0.75 / 25) * 25;
+            $parallaxImage.$container.style.height = (Math.ceil(window.innerHeight * 0.75 / 25) * 25) + 'px';
         });
     };
 
     var scroll = function() {
         [].forEach.call($parallaxImages, function($parallaxImage, i) {
-            var diff = $parallaxImage.$container.offsetTop - window.scrollY;
-            var yPos = Math.round(0.5 * diff) - Math.round(window.innerHeight * 0.125)
-            
-            $parallaxImage.style.transform = 'translate3d(0,' + yPos + 'px,0)';
-            $parallaxImage.style.zIndex = (diff + $parallaxImage.$container.height <= 0 ) ? -1 : 0;
+            var scrolledTo = ($parallaxImage.$container.offsetTop - window.innerHeight) <= window.scrollY;
+            var scrolledPast = ($parallaxImage.$container.offsetTop + $parallaxImage.$container.offsetHeight) < window.scrollY;
+
+            if (scrolledTo && !scrolledPast) {
+                var yPos = Math.round(0.5 * ($parallaxImage.$container.offsetTop - window.scrollY));
+
+                $parallaxImage.style.transform = 'translate3d(0,' + yPos + 'px,0)';
+
+                var scrolledHalfTo = ($parallaxImage.$container.offsetTop - window.innerHeight / 2) <= window.scrollY;
+                var scrolledHalfPast = ($parallaxImage.$container.offsetTop + $parallaxImage.$container.offsetHeight - window.innerHeight / 2) < window.scrollY;
+
+                var zIndex = -1;
+                if (scrolledHalfTo) {
+                    zIndex = 0;
+                }
+                if (scrolledHalfPast) {
+                    zIndex = -1 * i;
+                }
+                $parallaxImage.style.zIndex = zIndex;
+                $parallaxImage.style.visibility = 'visible';
+            } else {
+                $parallaxImage.style.visibility = 'hidden';
+            }
         });
         window.requestAnimationFrame(scroll);
     };
